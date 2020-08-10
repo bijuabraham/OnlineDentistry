d5821410cd1ada1af3c353e22c4bf2f80d5d91f2
Biju Abraham <bijuabraham@bijus-mbp.attlocal.net>
2020-08-10 16:31:31 -0700

    Added flush

--- certgen.php
+++ certgen.php
@@ -1,67 +1,62 @@
 <?php
-ini_set("display_errors","1");
+ini_set("display_errors","0");
 ERROR_REPORTING(E_ALL);
 require_once 'fpdf.php';
 session_start();
-
+putenv('GDFONTPATH=' . realpath('.'));
+define('FPDF_FONTPATH',realpath('.'));
 function generateCertificate ($id, $name, $subject, $by, $on) {
-	#header ("Content-type: image/png");
-	$tmpPNGFile = "certs/" . $id . "_certificate.png";
 	$tmpPDFFile = "certs/" . $id . "_certificate.pdf";
 	// Set the environment variable for GD
-	putenv('GDFONTPATH=' . realpath('.'));
 	// Name the font to be used (note the lack of the .ttf extension)
 	#$font = 'arial.ttf';
-	$font = 'MonospaceTypewriter.ttf';
-	$bigsize = 40;
-	$smallsize = 30;
-	$pad_length = 41;
-	$pad_length_small = 53;
+	$font = 'monospacetypewriter';
+	$bigsize = 20;
+	$smallsize = 16;
+	$pad_length = 43;
+	$pad_length_small = 52;
 	$pad_type = STR_PAD_BOTH;
 	$pad_string = " ";
 	
 	#Elements to embed
 	#$name = "BIJU ABRAHAM";
-	#$name = "01234567890123456789012345678901234567890123";
+	#$name = "0123456789012345678901234567890123456789012";
 	$name = str_pad ( $name , $pad_length , $pad_string , $pad_type);
 	
 	#$subject = "Orthodontics Dental Class";
-	#$subject = "01234567890123456789012345678901234567890123";
+	#$subject = "0123456789012345678901234567890123456789012";
 	$subject = str_pad ( $subject , $pad_length , $pad_string , $pad_type);
 	
 	#$by = "By " . "Dr. Joe Antony";
-	#$by = "012345678901234567890123456789012345678901234567890123456789";
+	#$by = "0123456789012345678901234567890123456789012345678901";
 	$by = str_pad ( $by , $pad_length_small , $pad_string , $pad_type);
 	
 	#$on = "On " . "August 8, 2020";
-	#$on = "012345678901234567890123456789012345678901234567890123456789";
+	#$on = "0123456789012345678901234567890123456789012345678901";
 	$on = str_pad ( $on , $pad_length_small , $pad_string , $pad_type);
 	
-	# Image handling
-	$img_handle = imageCreateFromPNG("certbgA4.png");
-	$blue = imagecolorallocate($img_handle, 0, 0, 255);
-	$black = imagecolorallocate($img_handle, 0, 0, 0);
-	$red = imagecolorallocate($img_handle, 200, 45, 10);
-	$color = ImageColorAllocate ($img_handle, 0, 0, 0);
-	#imagettftext ( $image , $size , $angle , int $x , int $y , int $color , string $fontfile , string $text ) : array
-	imagettftext ( $img_handle , $bigsize , 0 , 0 , 680 , $blue , $font , $name );
-	imagettftext ( $img_handle , $bigsize , 0 , 0 , 880 , $red , $font , $subject );
-	imagettftext ( $img_handle , $smallsize , 0 , 0 , 940 , $black , $font , $by );
-	imagettftext ( $img_handle , $smallsize , 0 , 0 , 1000 , $black , $font , $on );
-	#Destroy
-	ImagePng ($img_handle, $tmpPNGFile);
-	#Convert
-	$pdf = new FPDF();
+    # PDF handling
+    $pdf = new FPDF();
 	$pdf->AddPage('P','A4');
 	$pdf->SetAuthor('Online Dentistry');
-	$pdf->SetTitle('Online Dentistry Certificate');
-	$pdf->Image($tmpPNGFile,5,0,200);
+    $pdf->SetTitle('Online Dentistry Certificate');
+    $pdf->Image("certbgA4.png",5,0,200);
+    $pdf->AddFont($font);
+    $pdf->SetFont($font, "", $bigsize);
+    $pdf->SetTextColor(0,0,255); #blue
+    $pdf->SetXY(18, 105);
+    $pdf->Write(1,$name);
+    $pdf->SetTextColor(200,45,10); #red
+    $pdf->SetXY(18, 135);
+    $pdf->Write(1,$subject);
+    $pdf->SetFont($font, "", $smallsize);
+    $pdf->SetTextColor(0,0,0); #black
+    $pdf->SetXY(18, 150);
+    $pdf->Write(1,$by);
+    $pdf->SetXY(18, 160);
+    $pdf->Write(1,$on);
 	$pdf->Output($tmpPDFFile,'F');
-	$pdf->close();
-	
-	#Destroy
-	
-	ImageDestroy ($img_handle);
+    $pdf->close();
 	if (file_exists ( $tmpPDFFile )) {
 		return TRUE;
 	 } else {
--- certmailer.php
+++ certmailer.php
@@ -1,4 +1,5 @@
 <?php
+    ob_start();
     ini_set("display_errors","0");
     ERROR_REPORTING(E_ALL);
     require_once 'mailer.php';
@@ -24,130 +25,131 @@
 
 if ($is_send == '' || $is_send == 'Cancel') {
 ?>
-<table width="65%" border="0" cellpadding="0" cellspacing="0">
-  <tr>
-    <td bgcolor="e5ecf9" class="forumposts">
-<form method="post" action="certmailer.php">
-<label for="Message">Email Message:</label><BR>
-<textarea id="message" name="message" rows="10" cols="50">
-Hello,<BR>
-<BR>
-Attached is your Online Dentistry Patricipation Certificate.<BR>
-Thank you for attending the class.<BR>
-<BR>
-<B>Administratior</B><BR>
-<I>Online Dentistry</I><BR>
-<BR>
-</textarea><BR><BR>
-<label for="title">Course Title:</label><br>
-<input type="text" name="title" id="title" value="Orthodonics Course"><br><br>
-<label for="by">Instructor name:</label><br>
-<input type="text" name="by" id="by" value="By Dr. Binu Abraham"><br><br>
-<label for="on">Course date:</label><br>
-<input type="text" name="on" id="on" value="August 8, 2020"><br><br>
-<label for="startcert">Starting Certificate Line:</label><br>
-<input type="number" name="startcert" id="startcert" value="0"><br><br>
-<label for="endcert">Ending Certificate Line:</label><br>
-<input type="number" name="endcert" id="endcert" value="0"><br><br>
-<input type="submit" name="Send" value="Review">
-</form>
-</td>
-</tr>
-</table>
-<?php
+  <table width="65%" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+      <td bgcolor="e5ecf9" class="forumposts">
+  <form method="post" action="certmailer.php">
+  <label for="Message">Email Message:</label><BR>
+  <textarea id="message" name="message" rows="10" cols="50">
+  Hello,<BR>
+  <BR>
+  Attached is your Online Dentistry Patricipation Certificate.<BR>
+  Thank you for attending the class.<BR>
+  <BR>
+  <B>Administratior</B><BR>
+  <I>Online Dentistry</I><BR>
+  <BR>
+  </textarea><BR><BR>
+  <label for="title">Course Title:</label><br>
+  <input type="text" name="title" id="title" value="Orthodonics Course"><br><br>
+  <label for="by">Instructor name:</label><br>
+  <input type="text" name="by" id="by" value="By Dr. Binu Abraham"><br><br>
+  <label for="on">Course date:</label><br>
+  <input type="text" name="on" id="on" value="August 8, 2020"><br><br>
+  <label for="startcert">Starting Certificate Line:</label><br>
+  <input type="number" name="startcert" id="startcert" value="0"><br><br>
+  <label for="endcert">Ending Certificate Line:</label><br>
+  <input type="number" name="endcert" id="endcert" value="0"><br><br>
+  <input type="submit" name="Send" value="Review">
+  </form>
+  </td>
+  </tr>
+  </table>
+  <?php
 }
 if (($is_send == 'Send') && $message && user_admin($user)) {
-    foreach(glob("certs/*") as $f) {
-      if( $f == "certs/*.p*") continue;
-      unlink($f);
+  ob_flush();
+  foreach(glob("certs/*") as $f) {
+    if ($f == "certs/*.p*") continue;
+    unlink($f);
+  }
+  db_connect("CCDB");
+  if ($message != "") {
+    $emailquery = "select studentid as studentid, sendemail as sendemail, firstname as FirstName, lastname as LastName, certname as certname from mailinglist";
+    //print $sqlquery2;
+    $resultemails = db_fetch_all("CCDB", $emailquery);
+    $num=db_num_rows($resultemails);
+    if ($endcert > $num) {$endcert = $num;}
+    $subject = "Online Dentistry - Certificate";
+    if ($external) {
+      echo "Generating and <font color=\"red\">EMAILING</font> Certificates from $startcert to $endcert<BR><BR>";
+    } else {
+      echo "Generating and Testing Certificates from $startcert to $endcert<BR><BR>";
     }
-    db_connect("CCDB");
-    if ($message != "") {
-        $emailquery = "select studentid as studentid, sendemail as sendemail, firstname as FirstName, lastname as LastName, certname as certname from mailinglist";
-        //print $sqlquery2;
-        $resultemails = db_fetch_all("CCDB", $emailquery);
-        $num=db_num_rows($resultemails);
-        if ($endcert > $num) {$endcert = $num;}
-        $subject = "Online Dentistry - Certificate";
-        if ($external) {
-          echo "Generating and <font color=\"red\">EMAILING</font> Certificates from $startcert to $endcert<BR><BR>";
-        } else {
-          echo "Generating and Testing Certificates from $startcert to $endcert<BR><BR>";
+    for ($i = $startcert; $i <= $endcert; $i++) {
+        //attachment 
+        $recipient=db_result($resultemails,$i,"sendemail");
+        $certname=db_result($resultemails,$i,"certname");
+        $studentid=db_result($resultemails,$i,"studentid");
+        #function sendPHPMailer($toaddress, $toname, $subject, $attach, $message) {
+        $sentMailResult = FALSE;
+        #echo $i . " : " . $studentid . "," . $certname . "," . $title . "," . $by . "," . $on . "," . $external . "<BR>";
+        if(generateCertificate ($studentid, $certname, $title, $by, $on)) {
+          #$sentMailResult = sendPHPMailer($studentid, $recipient, $certname, $subject, 1, $message, $external);
         }
-        for ($i = $startcert; $i <= $endcert; $i++) {
-            //attachment 
-            $recipient=db_result($resultemails,$i,"sendemail");
-            $certname=db_result($resultemails,$i,"certname");
-            $studentid=db_result($resultemails,$i,"studentid");
-            
-            #function sendPHPMailer($toaddress, $toname, $subject, $attach, $message) {
-            $sentMailResult = FALSE;
-            #echo $i . " : " . $studentid . "," . $certname . "," . $title . "," . $by . "," . $on . "," . $external;
-            if(generateCertificate ($studentid, $certname, $title, $by, $on)) {
-              $sentMailResult = sendPHPMailer($studentid, $recipient, $certname, $subject, 1, $message, $external);
-            }
-            if($sentMailResult)  
-              { 
-                echo $studentid . ":" . $certname . " - Success<BR>"; 
-              } 
-              else
-              { 
-                echo($studentid . ":" . $certname . " - <font color=\"red\">Failed</font><BR>"); 
-              } 
-          }
+        if($sentMailResult)  
+        { 
+          echo $studentid . ":" . $certname . " - Success<BR>"; 
+        } 
+        else
+        { 
+          echo($studentid . ":" . $certname . " - <font color=\"red\">Failed</font><BR>"); 
+        }
+        ob_flush();
       }
-      db_close("CCDB");
-      print "<BR>Emails sent Successfully. <BR><font color=red>Do not refresh or resend from this Screen.</font>";
+    }
+  db_close("CCDB");
+  print "<BR>Emails sent Successfully. <BR><font color=red>Do not refresh or resend from this Screen.</font>";
 }
 
 if (($is_send == 'Review') && $message)
 {
   db_connect("CCDB");
-?>
-<table width="65%" border="0" cellpadding="0" cellspacing="0">
-  <tr>
-    <td bgcolor="e5ecf9" class="forumposts">
-<form name="form2" method="post" action="certmailer.php" style="padding:5px;">
-<p><br>
-<B>Review the Message: </B><BR />
-<BR><?php echo $message; ?><BR>
-<BR><B>Title: </B><?php echo $title; ?><BR>
-<B>By: </B><?php echo $by; ?><BR>
-<B>On: </B><?php echo $on; ?><BR>
-<B>Starting Certificate row: </B><?php echo $startcert; ?> to <?php echo $endcert; ?></BR>
-<input type="hidden" name="message" value="<?php echo $message; ?>">
-<input type="hidden" name="title" value="<?php echo $title; ?>">
-<input type="hidden" name="by" value="<?php echo $by; ?>">
-<input type="hidden" name="on" value="<?php echo $on; ?>"><BR>
-<input type="hidden" name="startcert" value="<?php echo $startcert; ?>"><BR>
-<input type="hidden" name="endcert" value="<?php echo $endcert; ?>"><BR>
-<input type="checkbox" id="external" name="external" checked><label for="external"> SEND EXTERNAL EMAILS</label><BR>
-<?php
-    $queryasofdate ="select date from AS_OF_DATE";
-    $result = db_fetch_all("CCDB", $queryasofdate);
-    $lastuploaddate = db_result($result,0,"date");
-    if (!$lastuploaddate) {
-        echo "AS_OF_DATE Query failed<BR>\n";
-    } else {
-        echo "Last upload date of Participants: $lastuploaddate <BR>\n";
-    }
-    $mailinglistquery ="select count(*) as count from mailinglist";
-    $result = db_fetch_all("CCDB", $mailinglistquery);
-    $participantcount = db_result($result,0,"count");
-    if (!$participantcount) {
-      echo "No Participant List found or Query failed.<BR>\n";
-    } else {
-      echo "Total Participants: $participantcount <BR><BR>\n";
+  ?>
+  <table width="65%" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+      <td bgcolor="e5ecf9" class="forumposts">
+  <form name="form2" method="post" action="certmailer.php" style="padding:5px;">
+  <p><br>
+  <B>Review the Message: </B><BR />
+  <BR><?php echo $message; ?><BR>
+  <BR><B>Title: </B><?php echo $title; ?><BR>
+  <B>By: </B><?php echo $by; ?><BR>
+  <B>On: </B><?php echo $on; ?><BR>
+  <B>Starting Certificate row: </B><?php echo $startcert; ?> to <?php echo $endcert; ?></BR>
+  <input type="hidden" name="message" value="<?php echo $message; ?>">
+  <input type="hidden" name="title" value="<?php echo $title; ?>">
+  <input type="hidden" name="by" value="<?php echo $by; ?>">
+  <input type="hidden" name="on" value="<?php echo $on; ?>"><BR>
+  <input type="hidden" name="startcert" value="<?php echo $startcert; ?>"><BR>
+  <input type="hidden" name="endcert" value="<?php echo $endcert; ?>"><BR>
+  <input type="checkbox" id="external" name="external" checked><label for="external"> SEND EXTERNAL EMAILS</label><BR>
+  <?php
+  $queryasofdate ="select date from AS_OF_DATE";
+  $result = db_fetch_all("CCDB", $queryasofdate);
+  $lastuploaddate = db_result($result,0,"date");
+  if (!$lastuploaddate) {
+      echo "AS_OF_DATE Query failed<BR>\n";
+  } else {
+      echo "Last upload date of Participants: $lastuploaddate <BR>\n";
   }
-    ?>
-          <input type="submit" name="Send" value="Cancel">&nbsp;&nbsp;
-          <input type="submit" name="Send" value="Send">
-        </p>
-      </form>
-      </td>
-</tr>
-</table>
-    <?php
+  $mailinglistquery ="select count(*) as count from mailinglist";
+  $result = db_fetch_all("CCDB", $mailinglistquery);
+  $participantcount = db_result($result,0,"count");
+  if (!$participantcount) {
+    echo "No Participant List found or Query failed.<BR>\n";
+  } else {
+    echo "Total Participants: $participantcount <BR><BR>\n";
+  }
+  ?>
+  <input type="submit" name="Send" value="Cancel">&nbsp;&nbsp;
+  <input type="submit" name="Send" value="Send">
+  </p>
+  </form>
+  </td>
+  </tr>
+  </table>
+  <?php
 }
 ?>
 <p>
@@ -156,4 +158,6 @@ if (($is_send == 'Review') && $message)
 <?php
 db_close("CCDB");
 od_footer();
+ob_flush();
+ob_end_flush();
 ?>
